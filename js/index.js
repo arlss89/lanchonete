8937b562c2f046aa88139a10d4131ea04140778e
Everton-Moraes <evertongmoraes@gmail.com>
2020-07-11 08:26:40 -0300

    Update index.js
    Correção do opção de bebida

--- js/index.js
+++ js/index.js
@@ -8,33 +8,37 @@ window.onload = function(){
         document.querySelector("#S").disabled = false
         document.querySelector("#N").disabled = false
         document.querySelector("#idlanche").disabled = false
-        document.querySelector("#Si").disabled = false
-        document.querySelector("#Na").disabled = false
+        
+        
         document.querySelector("#iddata").disabled = false
         document.querySelector("#Sim").disabled = false
         document.querySelector("#Nao").disabled = false
         document.querySelector("#idobs").disabled = false
         document.querySelector("#idSim").disabled = false
     })
-
+    //Comando para Habilitar os tipos de bebidas
     document.querySelector('#S').addEventListener('click', function(){
         if(document.querySelector('#S').checked == true){
             document.querySelector('#idtpbebida').disabled = false
+            document.querySelector("#Si").disabled = false
+            document.querySelector("#Na").disabled = false
         }
     })
-        
+    //Comando para Desabilitar os tipos de bebidas
     document.querySelector('#N').addEventListener('click', function(){
         if(document.querySelector('#N').checked == true){
             document.querySelector('#idtpbebida').disabled = true
+            document.querySelector("#Si").disabled = true
+            document.querySelector("#Na").disabled = true
         }
     })
-
+    //Comnado para mostrar a lista de opcionais
     document.querySelector('#Sim').addEventListener('click', function(){
         if(document.querySelector('#Sim').checked == true){
             document.querySelector('#opadicionais').style.display = 'block'
         }
     })
-        
+    //Comando para ocultar a lista de opcionais    
     document.querySelector('#Nao').addEventListener('click', function(){
         if(document.querySelector('#Nao').checked == true){
             document.querySelector('#opadicionais').style.display = 'none'
